5454ef4763874d6bb96360af44b9d6a49b6025c2
Alejandro Marin <ingenieroalejandromarin@gmail.com>
2023-11-24 08:31:00 -0500

    Ejercicios GO

---
+++ rpsweb/handlers/handlers.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+const (
+	templateDir  = "templates/"
+	templateBase = templateDir + "base.html"
+)
+
+type Player struct {
+	Name string
+}
+
+var player Player
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	restarValue()
+	renderTemplate(w, "index.html", nil)
+}
+
+func NewGame(w http.ResponseWriter, r *http.Request) {
+	restarValue()
+	renderTemplate(w, "new-game.html", nil)
+}
+
+func Game(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
+
+		player.Name = r.Form.Get("name")
+	}
+
+	//Redireciona a otra ruta
+	if player.Name == "" {
+		http.Redirect(w, r, "/new", http.StatusFound)
+	}
+
+	renderTemplate(w, "game.html", player)
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Jugar")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	restarValue()
+	renderTemplate(w, "about.html", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string, data any) {
+	tpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error al renderizar plantilla", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+}
+
+// Reiniciar valores
+func restarValue() {
+	player.Name = ""
+}
---
+++ rpsweb/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"rpsweb/handlers"
+)
+
+func main() {
+	//Crear enrutador
+	router := http.NewServeMux()
+
+	//Manejador para servir los archivos estaticos
+	fs := http.FileServer(http.Dir("static"))
+
+	//Ruta para acceder a los archivos estaticos
+	router.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	//Configurar rutas
+	router.HandleFunc("/", handlers.Index)
+	router.HandleFunc("/new", handlers.NewGame)
+	router.HandleFunc("/game", handlers.Game)
+	router.HandleFunc("/play", handlers.Play)
+	router.HandleFunc("/about", handlers.About)
+
+	port := ":8080"
+	fmt.Printf("El servidor escuchando en http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
